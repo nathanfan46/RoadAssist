ce29328a6ad8c33420c1c6672ffb6d2cad95eb7f
Nathan Fan <nathanfan46@gmail.com>
2017-01-17 04:07:31 +0800

    Init setting

---
+++ resources/views/roadassist.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.basic')
+
+@section('view_scripts')
+<!-- <script src="{{ asset("Pratt/assets/js/bootstrap.js") }}"></script> -->
+<script src="/js/view/profile.js"></script>
+@endsection
